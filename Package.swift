cdcc2be1bbf616025b93b3c133fb9fd242f2c440
Mitch Treece <mitchtreece@me.com>
2026-06-04 17:17:52 -0700

    Now targeting macOS v15

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Espresso",
     platforms: [
         .iOS(.v18),
-        .macOS(.v10_15)
+        .macOS(.v15)
     ],
     products: [
         .library(for: .espresso)
